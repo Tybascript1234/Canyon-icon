bf7170d9d004f96036dba0dc66430d0a22fc2e69
Tybascript1234 <tybascript@gmail.com>
2025-04-03 20:33:34 +0300

    Update gallery.js

--- gallery.js
+++ gallery.js
@@ -126,7 +126,7 @@ document.addEventListener("DOMContentLoaded", async () => {
                 resultItem.setAttribute("onmousedown", "createRipple(event)");
                 resultItem.innerHTML = `
                     <div class="result-item-div">
-                        <div class="icon-container"><ion-icon name="search-outline"></ion-icon></div>
+                        <div class="icon-container"><svg xmlns="http://www.w3.org/2000/svg" height="22px" viewBox="0 -960 960 960" width="22px" fill="#1f1f1f"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg></div>
                         <div class="image-name">${name}</div>
                     </div>
                     <div>
@@ -217,7 +217,7 @@ document.addEventListener("DOMContentLoaded", async () => {
         overlay.style.left = 0;
         overlay.style.width = '100%';
         overlay.style.height = '100%';
-        overlay.style.backgroundColor = 'rgb(0 0 0 / 5%)';
+        overlay.style.backgroundColor = '#00000021';
         overlay.style.zIndex = 999;
         
         // إضافة الطبقة فقط إذا كان العرض 800 بكسل أو أقل
